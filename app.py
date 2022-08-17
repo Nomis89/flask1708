a30ef985ee8adce933033a282a6ec55769b57ccd
Nomis89 <95224816+Nomis89@users.noreply.github.com>
2022-08-17 15:47:42 +0200

    tweede

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 def hello_world():
     print("fasdf")
     return "<p>Hello, World!</p>"
+
+@app.route("/tweede")
+def bye_world():
+    print("fasdf")
+    return "<p>Bye, World!</p>"
